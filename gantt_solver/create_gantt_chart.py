98123c43472b18323bcae61e4f2fde8ee485f739
Jon Minter <jdiminter@gmail.com>
2021-12-12 20:51:26 -0500

    Adding functionality to collect multiple solutions if there are more than one

--- gantt_solver/create_gantt_chart.py
+++ gantt_solver/create_gantt_chart.py
@@ -3,16 +3,18 @@ from __future__ import annotations
 # Import libraries
 import collections
 import ortools.sat.python.cp_model
-from ortools.sat.python.cp_model import CpModel
+from ortools.sat.python.cp_model import CpModel, CpSolverSolutionCallback
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
-from typing import List
+from typing import List, Dict
 from graphlib import TopologicalSorter
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 import json
+import jsons
 import sys
 import argparse
 import jsonschema
+import heapq
 
 @dataclass
 class Project:
@@ -30,6 +32,63 @@ class Project:
 class Dependency:
     project: Project
     lag_time: object
+
+@dataclass
+class ProjectSchedule:
+    project_id: str
+    project_name: str
+    num_resources: int
+    start: int
+    end: int
+
+@dataclass(order=True)
+class ProjectSchedulingSolution:
+    total_duration: int
+    project_schedules: List[ProjectSchedule] = field(compare=False)
+
+class SolutionCollector(CpSolverSolutionCallback):
+    def __init__(self, projects: Dict[str, Project], limit: int):
+        CpSolverSolutionCallback.__init__(self)
+        self.__projects = projects
+        self.__solution_count = 0
+        self.__solution_limit = limit
+        self.__solutions = []
+
+    def on_solution_callback(self):
+        self.__solution_count += 1
+
+        total_duration = self.ObjectiveValue()
+
+        project_schedules = []
+        for project_id, project in self.__projects.items():
+            project_schedules.append(ProjectSchedule(
+                project_id,
+                project.name,
+                self.Value(project.num_resources),
+                self.Value(project.start),
+                self.Value(project.end),
+            ))
+
+        # Treat __solutions as a heap insert with schedules with shortest durations at head of list
+        heapq.heappush(
+            self.__solutions,
+            ProjectSchedulingSolution(int(total_duration), project_schedules))
+
+    def solution_count(self) -> int:
+        return self.__solution_count
+    
+    def top_solutions(self) -> List[ProjectSchedulingSolution]:
+        """
+        Return the first {self.__solution_limit} solutions ordered by total duration of the schedule
+
+        TODO: If storing all solutions in memory is ever an issue can
+              use heapq.nsmallest to trucate heap in on_solution_callback if the
+              heap exeeds {self.__solution_limit}. We can do this because it's a min
+              heap and we know that the smallest item we could have discarded would
+              still be greater than the largest item in the heap
+        """
+        return heapq.nsmallest(self.__solution_limit, self.__solutions)
+
 
 
 INPUT_SCHEMA = {
@@ -69,16 +128,62 @@ INPUT_SCHEMA = {
     }
 }
 
+def save_solution_json(project_scheduling_solution: ProjectSchedulingSolution, output_file: str):
+    with open(output_file, mode="w") as f:
+        f.write(jsons.dumps(project_scheduling_solution, indent=True))
+
+def create_gantt_chart(project_scheduling_solution: ProjectSchedulingSolution, output_file: str):
+    bars = []
+
+    project_schedules_ordered_by_start = project_scheduling_solution.project_schedules
+    project_schedules_ordered_by_start.sort(key=lambda p: p.start, reverse=True)
+
+    for project_schedule in project_schedules_ordered_by_start:
+        project_start = project_schedule.start
+        # project_end = project_schedule.end
+        num_resources = project_schedule.num_resources
+        bars.append((project_schedule.project_name,num_resources, (project_start, project_schedule.end - project_schedule.start)))
+
+    # Plot gantt chart
+    fig, gnt = plt.subplots(figsize=(12, 8))
+    fig.suptitle('Gantt Chart', fontsize=16)
+    gnt.set_xlabel('Start/Duration') 
+    gnt.set_ylabel('Projects') 
+    gnt.set_yticks([12 + i * 10 for i in range(len(bars))]) 
+    gnt.set_yticklabels([bar[0] for bar in bars]) 
+    gnt.grid(True)
+    allcolors=[
+        'tab:orange',
+        'tab:green',
+        'tab:red',
+        'tab:purple',
+        'tab:blue',
+        'tab:pink',
+        'tab:black'
+    ]
+    # Loop bars
+    for i in range(len(bars)):
+        gnt.broken_barh([bars[i][2]], (10 + i * 10, 4), facecolors=(allcolors[i % len(allcolors)], 'tab:grey'))
+        j = 0
+        for x1, x2 in [bars[i][2]]:
+            gnt.text(x=x1 + x2/2, y= 12 + i * 10, s=bars[i][1], ha='center', va='center', color='white')
+            j += 1
+
+    # save the plot
+    plt.savefig(output_file)
+
 
 def main():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("input_file", help="JSON file with project definitions")
-    arg_parser.add_argument("output_file", help="PNG file to save gantt chart visualization to")
+    arg_parser.add_argument("output_file", help="Prefix for solution JSON files and Gantt chart PNGs")
     arg_parser.add_argument("--timeout", default=30, type=int, help="Maximum time (in seconds) to allow gantt solver to run")
+    arg_parser.add_argument("--max-solutions", default=5, type=int, help="Maximum number of solutions to find before stopping")
     args = arg_parser.parse_args()
     inpput_json_file = args.input_file
-    output_png_file = args.output_file
+    output_file = args.output_file
     maximum_time_to_run = args.timeout
+    maximum_num_solutions = args.max_solutions
 
     # Input
     projects = {}
@@ -131,9 +236,9 @@ def main():
         for dependency in project.dependencies:
             model.Add(project.start >= dependency.project.end + dependency.lag_time)
     
-    # We can only use max resources at a time
+    # We can only use up to max resources at a time
     # Add a constraint that for every project interval sum num resources
-    # required the project. Ensures that at no singular point in the 
+    # required by the project. Ensures that at no singular point in the 
     # timeline are there projects active that require more resources
     # than we have available
     #
@@ -157,59 +262,22 @@ def main():
     # FIND A SOLUTION
     solver = ortools.sat.python.cp_model.CpSolver()
     solver.parameters.max_time_in_seconds = maximum_time_to_run
-    status = solver.Solve(model)
+    solution_collector = SolutionCollector(projects, maximum_num_solutions)
+    status = solver.Solve(model, solution_collector)
 
     # Print output if the solution is optimal/feasible
-    if (status == ortools.sat.python.cp_model.OPTIMAL or status == ortools.sat.python.cp_model.FEASIBLE):
-        # Print the solution
-        print('--- Final solution ---\n')
-        print('Optimal Schedule Length: {0}\n'.format(solver.ObjectiveValue()))
-        print('Schedules:')
-
-        bars = []
-        projects_sorted_by_start = list(projects.values())
-        projects_sorted_by_start.sort(key=lambda p: solver.Value(p.start), reverse=True)
-
-        for project in projects_sorted_by_start:
-            project_start = solver.Value(project.start)
-            project_end = solver.Value(project.end)
-            num_resources = solver.Value(project.num_resources)
-            bars.append((project.name,num_resources, (project_start, project.duration)))
-
-            print(project.name,':', project_start, ' -> ', project_end)
-                
-        
-        # Plot gantt chart
-        fig, gnt = plt.subplots(figsize=(12, 8))
-        fig.suptitle('Gantt Chart', fontsize=16)
-        gnt.set_xlabel('Start/Duration in Weeks') 
-        gnt.set_ylabel('Projects') 
-        gnt.set_yticks([12 + i * 10 for i in range(len(bars))]) 
-        gnt.set_yticklabels([bar[0] for bar in bars]) 
-        gnt.grid(True)
-        allcolors=[
-            'tab:orange',
-            'tab:green',
-            'tab:red',
-            'tab:purple',
-            'tab:blue',
-            'tab:pink',
-            'tab:black'
-        ]
-        # Loop bars
-        for i in range(len(bars)):
-            print("bar = ", bars[i])
-            gnt.broken_barh([bars[i][2]], (10 + i * 10, 4), facecolors=(allcolors[i], 'tab:grey'))
-            j = 0
-            for x1, x2 in [bars[i][2]]:
-                gnt.text(x=x1 + x2/2, y= 12 + i * 10, s=bars[i][1], ha='center', va='center', color='white')
-                j += 1
-
-        # Show or save the plot
-        #plt.show()
-        plt.savefig(output_png_file)
+    if solution_collector.solution_count() > 0:
+        print(f"Found solutions! Status = {solver.StatusName(status)}")
+        for i, solution in enumerate(solution_collector.top_solutions()):
+            json_file = f"{output_file}.solution{i}.json"
+            chart_file = f"{output_file}.solution{i}.png"
+            save_solution_json(solution, json_file)
+            create_gantt_chart(solution, chart_file)
+            print(f"Solution {i}:")
+            print(f"---- {json_file}")
+            print(f"---- {chart_file}")
     else:
-        print("No feasible solution found!")
+        raise RuntimeError("No feasible solution found!")
 
 
 # Tell python to run main method
